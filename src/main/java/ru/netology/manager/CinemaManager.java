760149ce24bc744e8440d790e4ef22a53febfa40
Yulia <seminajlj@rambler.ru>
2021-07-09 11:20:36 +0300

    Доработка по проекту, изменение Тест кейсов.

--- src/main/java/ru/netology/manager/CinemaManager.java
+++ src/main/java/ru/netology/manager/CinemaManager.java
@@ -1,60 +1,42 @@
 package ru.netology.manager;
 
-import lombok.NoArgsConstructor;
+
 import ru.netology.domain.Cinema;
 
-
-import java.util.concurrent.Callable;
-
-import static java.util.Objects.nonNull;
-
-@NoArgsConstructor
 public class CinemaManager {
     private Cinema[] films = new Cinema[0];
-    int filmsLimit = 10;
+    private int returnFilms;
 
-    public CinemaManager(int filmsQuantity) {
-        this.filmsLimit = filmsQuantity;
+    public CinemaManager(int returnFilms) {
+
+        this.returnFilms = returnFilms;
+    }
+
+    public Cinema[] getCinema() {
+        int length = returnFilms;
+        if (length > films.length) {
+            length = films.length;
+        }
+
+        Cinema[] result = new Cinema[length];
+
+        for (int i = 0; i < length; i++) {
+            int index = films.length - i - 1;
+            result[i] = films[index];
+        }
+        return result;
     }
 
     public void add(Cinema film) {
         int length = films.length + 1;
         Cinema[] tmp = new Cinema[length];
-        // itar + tab
-        // копируем поэлементно
-        // for (int i = 0; i < items.length; i++) {
-        //   tmp[i] = items[i];
-        // }
-        System.arraycopy(films, 0, tmp, 0, films.length);
-        // кладём последним наш элемент
+
+        for (int i = 0; i < films.length; i++) {
+            tmp[i] = films[i];
+        }
+
         int lastIndex = tmp.length - 1;
         tmp[lastIndex] = film;
         films = tmp;
-
-    }
-
-    public Cinema[] getAll() {
-        Cinema[] result = new Cinema[films.length];
-        // перебираем массив в прямом порядке
-        // но кладём в результаты в обратном
-        for (int i = 0; i < result.length; i++) {
-            int index = films.length - 1 - i;
-            result[i] = films[index];
-        }
-        return result;
-    }
-
-
-    public Cinema[] getMovie() {
-        int count = filmsLimit;
-        if (films.length < count) {
-            count = films.length;
-        }
-        Cinema[] result = new Cinema[count];
-        for (int i = 0; i < result.length; i++) {
-            int index = count - i - 1;
-            result[i] = films[index];
-        }
-        return result;
     }
 }
